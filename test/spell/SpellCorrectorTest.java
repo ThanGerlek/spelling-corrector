19d66346aef985b9f4e287d4c649a22fa60f3442
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-09-10 11:46:07 -0700

    test(spell): create SpellCorrectorTest

---
+++ test/spell/SpellCorrectorTest.java
@@ -0,0 +1,22 @@
+package spell;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SpellCorrectorTest {
+
+    private SpellCorrector spell;
+
+    @BeforeEach
+    void setUp() {
+        spell = new SpellCorrector();
+    }
+
+    @Test
+    void useDictionary() {
+    }
+
+    @Test
+    void suggestSimilarWord() {
+    }
+}
