335d6a8e3994706d9559f8ea746e6dea64942740
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-09-11 15:21:39 -0700

    test(edit): update EditDistanceSetGeneratorTest to use test char set

--- test/spell/editdistance/EditDistanceSetGeneratorTest.java
+++ test/spell/editdistance/EditDistanceSetGeneratorTest.java
@@ -5,11 +5,42 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 class EditDistanceSetGeneratorTest {
+
+    static EditDistanceSetGenerator getTestGenerator() {
+        Set<Character> testCharSet = getTestCharSet();
+
+        EditDistanceSetGenerator setGenerator =
+                new EditDistanceSetGenerator() {
+                    @Override
+                    ArrayList<EditGenerator> getGenerators() {
+                        ArrayList<EditGenerator> generators = super.getGenerators();
+                        setEditGeneratorsToUseCharSet(generators, testCharSet);
+                        return generators;
+                    }
+                };
+
+        return setGenerator;
+    }
+
+    static void setEditGeneratorsToUseCharSet(
+            ArrayList<EditGenerator> generators, Set<Character> charSet) {
+        for (EditGenerator generator : generators) {
+            generator.useCharSet(charSet);
+        }
+    }
+
+    static Set<Character> getTestCharSet() {
+        Set<Character> testCharSet = new HashSet<>();
+        testCharSet.add('a');
+        testCharSet.add('b');
+        return testCharSet;
+    }
 
     @Test
     void testGenerate() throws IOException {
