a8192355a3feaedd259514297897beb2986d9d80
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-09-11 16:56:09 -0700

    feat(spell): implement MostFrequentWordFinder

--- src/spell/MostFrequentWordFinder.java
+++ src/spell/MostFrequentWordFinder.java
@@ -1,5 +1,7 @@
 package spell;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 public class MostFrequentWordFinder {
@@ -19,8 +21,53 @@ public class MostFrequentWordFinder {
     }
 
     public void findInSet(Set<String> potentialWords) {
-        // TODO implement findInSet()
-        return;
+        Set<String> realWords = getRealWords(potentialWords);
+        if (realWords.size() == 0) {
+            closestWord = null;
+        } else {
+            closestWord = getMostFrequentWord(realWords);
+        }
     }
 
+    private Set<String> getRealWords(Set<String> potentialWords) {
+        Set<String> realWords = new HashSet<>();
+        for (String word : potentialWords) {
+            if (dictionary.containsWord(word)) {
+                realWords.add(word);
+            }
+        }
+        return realWords;
+    }
+
+    private String getMostFrequentWord(Set<String> words) {
+
+        ArrayList<String> currentWinners = new ArrayList<>();
+        int currentHighestCount = 0;
+
+        for (String word : words) {
+            int count = dictionary.getFrequencyCount(word);
+            if (count > currentHighestCount) {
+                currentWinners.clear();
+                currentWinners.add(word);
+                currentHighestCount = count;
+            } else if (count == currentHighestCount) {
+                currentWinners.add(word);
+            }
+        }
+
+        return getFirstAlphabetically(currentWinners);
+    }
+
+    private String getFirstAlphabetically(ArrayList<String> words) {
+        if (words.size() == 1) {
+            return words.get(0);
+        }
+        String currentBest = words.get(0);
+        for (int i = 1; i < words.size(); i++) {
+            if (words.get(i).compareTo(currentBest) > 0) {
+                currentBest = words.get(i);
+            }
+        }
+        return currentBest;
+    }
 }
